9cfb83c564926dd660b9f6aded7e30c88bcf78a4
leorpedroso <leorpedroso97@gmail.com>
2021-10-06 23:37:09 -0300

    Update answers

--- resposta1.cpp
+++ resposta1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "ex-1.cpp"
 #include <utility> // swap()
-#include <vector>
+#include <string>
 
 template<typename E>
 void sort(E* container, int size) {
@@ -16,18 +16,28 @@ void sort(E* container, int size) {
 }
 
 int main() {
+	// Exercicio 1
 	// Originalmente eh chamado o toString() de Pessoa
 	// Mudando o metodo toString() de Pessoa para virtual resolve isto
-	// Pessoa* p1 = new Pessoa("Ana Paula", Pessoa::FEMININO, time(0));
-  // Pessoa* p2 = new Aluno("Jose Silva", Pessoa::FEMININO, time(0), "01001010", Aluno::GRADUACAO);
+	Pessoa* p1 = new Pessoa("Ana Paula", Pessoa::FEMININO, time(0));
+  Pessoa* p2 = new Aluno("Jose Silva", Pessoa::FEMININO, time(0), "01001010", Aluno::GRADUACAO);
 	
-	// std::cout << p1->toString();
-  // std::cout << p2->toString();
+	std::cout << p1->toString();
+  std::cout << p2->toString();
+
+	// Exercicio 4
+	// Gera uma lista de 10 pessoas com um nome com duas letras iniciais aleatorias
+	// e entao ordena a lista em ordem alfabetica
+	srand (time(NULL));
 
 	Pessoa container[10];
 	for (int i=0; i<10; i++) {
-		container[i].set_nome(to_string(10-i));
-		// container[i] = Pessoa();
+		string initial = "";
+		char c = ('A' + rand()%26);
+		char d = ('a' + rand()%26);
+		initial += c;
+		initial += d;
+		container[i].set_nome(initial);
 		cout << container[i].toString() << endl;
 	}
 
